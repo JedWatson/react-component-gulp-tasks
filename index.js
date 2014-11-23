3147de21666105acf08ee5f0bea7af80b967abc6
Jed Watson <jed.watson@me.com>
2014-11-24 01:07:08 +1100

    Fixing up gulp version checking

--- index.js
+++ index.js
@@ -19,23 +19,49 @@ var browserify = require('browserify'),
 	source = require('vinyl-source-stream'),
 	watchify = require('watchify');
 
-try {
-	var projectGulpVersion = require(module.parent.paths[0] + '/gulp/package.json').version;
-} catch(e) {
-	
-}
-var packageGulpVersion = require('./node_modules/gulp/package.json').version;
-if (semver.satisfies(projectGulpVersion, '^' + packageGulpVersion)) {
-	console.log(
-		'\n' +
-		'\nYou do not have the correct version of Gulp installed in your project.' +
-		'\n' +
-		'\nPlease add gulp ^' + packageGulpVersion + ' to your package.json, npm install and try again.' +
-		'\n' +
-		'\n');
+/**
+ * Check that a compatible version of gulp is available in the project
+ */
+
+function fatal(err) {
+	var msg = '\n\n';
+	if (Array.isArray(err)) {
+		err.forEach(function(i) {
+			msg += i + '\n\n';
+		});
+	} else {
+		msg += (err || 'Fatal error, bailing.') + '\n\n';
+	}
+	console.log(msg);
 	process.exit(1);
 }
 
+try {
+	var projectGulpVersion = require(module.parent.paths[0] + '/gulp/package.json').version;
+} catch(e) {
+	// If we can't find gulp in the parent project, it's a fatal problem.
+	fatal(
+		'You do not seem to have Gulp installed in your project.',
+		'Please add gulp ^' + packageGulpVersion + ' to your package.json, npm install and try again.'
+	);
+}
+try {
+	// Check to make sure the local gulp and the project gulp match.
+	var packageGulpVersion = require('./node_modules/gulp/package.json').version;
+	if (semver.satisfies(projectGulpVersion, '^' + packageGulpVersion)) {
+		fatal(
+			'You do not have the correct version of Gulp installed in your project.',
+			'Please add gulp ^' + packageGulpVersion + ' to your package.json, npm install and try again.'
+		);
+	}
+} catch(e) {
+	// Assume gulp has been loaded from ../node_modules and it matches the requirements.
+}
+
+/**
+ * This package exports a function that binds tasks to a gulp instance
+ * based on the provided config.
+ */
 
 module.exports = function(gulp, config) {
 
