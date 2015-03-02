c70692268ad2bb34217cc1c29b539cea72585a7c
Jed Watson <jed.watson@me.com>
2015-03-02 22:49:29 +1100

    Adding example config.js

---
+++ example_config.js
@@ -0,0 +1,37 @@
+// Read the package.json to detect the package name and dependencies
+var pkg = JSON.parse(require('fs').readFileSync('./package.json'));
+
+// Get default dependencies from package.json.
+// Dependencies can be customised by hard-coding this array.
+var dependencies = [];
+Object.keys(pkg.dependencies).forEach(function(i) {
+	dependencies.push(i);
+});
+
+module.exports = {
+	
+	component: {
+		file: 'MyComponent.js',
+		name: 'MyComponent',
+		src: 'src',
+		dist: 'dist',
+		pkgName: pkg.name,
+		dependencies: dependencies
+	},
+	
+	example: {
+		src: 'example/src',
+		dist: 'example/dist',
+		files: [
+			'index.html',
+			'standalone.html'
+		],
+		scripts: [
+			'app.js'
+		],
+		stylesheets: [
+			'app.less'
+		]
+	}
+	
+};
