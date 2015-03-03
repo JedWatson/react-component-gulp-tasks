b4673ff07a2a331c94a01cc0a4894f353aab89d7
Jed Watson <jed.watson@me.com>
2015-03-03 23:03:43 +1100

    Adding readPackageJSON method

--- index.js
+++ index.js
@@ -54,3 +54,15 @@ module.exports = function(gulp, config) {
 	]);
 
 }
+
+module.exports.readPackageJSON = function readPackageJSON() {
+	var pkg = JSON.parse(require('fs').readFileSync('./package.json'));
+	var deps = [];
+	Object.keys(pkg.dependencies).forEach(function(i) {
+		deps.push(i);
+	});
+	return {
+		name: pkg.name,
+		deps: deps
+	};
+}
