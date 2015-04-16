baf7f09cd84601ceabaf9772928b20f0b6e86001
Jed Watson <jed.watson@me.com>
2015-04-16 13:11:11 +1000

    Supporting fallback to index.html for react-router

--- tasks/dev.js
+++ tasks/dev.js
@@ -1,10 +1,12 @@
 var connect = require('gulp-connect');
+var path = require('path');
 
 module.exports = function(gulp, config) {
 
 	gulp.task('dev:server', function() {
 		connect.server({
 			root: config.example.dist,
+			fallback: path.join(config.example.dist, 'index.html'),
 			port: 8000,
 			livereload: true
 		});
