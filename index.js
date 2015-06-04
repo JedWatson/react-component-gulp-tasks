c68957b14ffb17eeaae4f733847f93c845281e81
Daniel Cousens <github@dcousens.com>
2015-06-04 12:51:58 +1000

    remove global gulp support, gulp version check
    Again, this wasn't working anyway due to `semver` being undefined

--- index.js
+++ index.js
@@ -1,41 +1,6 @@
 var _ = require('lodash');
 
 /**
- * Check that a compatible version of gulp is available in the project
- */
-
-function fatal() {
-	var msg = '\n\n';
-	for (var i = 0; i < arguments.length; i++) {
-		msg += arguments[i] + '\n\n';
-	}
-	console.log(msg);
-	process.exit(1);
-}
-
-try {
-	var projectGulpVersion = require(module.parent.paths[0] + '/gulp/package.json').version;
-} catch(e) {
-	// If we can't find gulp in the parent project, it's a fatal problem.
-	fatal(
-		'You do not seem to have Gulp installed in your project.',
-		'Please add gulp ^' + packageGulpVersion + ' to your package.json, npm install and try again.'
-	);
-}
-try {
-	// Check to make sure the local gulp and the project gulp match.
-	var packageGulpVersion = require('./node_modules/gulp/package.json').version;
-	if (!semver.satisfies(projectGulpVersion, '^' + packageGulpVersion)) {
-		fatal(
-			'You have an incompatible version of Gulp installed (' + projectGulpVersion + ').',
-			'Please add gulp ^' + packageGulpVersion + ' to your package.json, npm install and try again.'
-		);
-	}
-} catch(e) {
-	// Assume gulp has been loaded from ../node_modules and it matches the requirements.
-}
-
-/**
  * Helper method to extract metadata from package.json
  */
 
@@ -81,7 +46,7 @@ function initTasks(gulp, config) {
 	if (!config.component.name) {
 		config.component.name = _.capitalize(_.camelCase(config.component.pkgName));
 	}
-	
+
 	if (!config.aliasify) {
 		config.aliasify = pkg.aliasify;
 	}
